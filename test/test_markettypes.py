5afdaa0da198a004af968d2c391173676e7d9e2f
leaen <mossbanay@gmail.com>
2018-02-14 15:25:37 +1100

    Support for Python3

--- test/test_markettypes.py
+++ test/test_markettypes.py
@@ -10,34 +10,34 @@ class TestOrder(unittest.TestCase):
         """
         o1 = Order(0, Side.BUY, 4.5, 10)
         o2 = Order(0, Side.BUY, 4.5, 10)
-        self.assertNotEquals(o1.orderid, o2.orderid)
+        self.assertNotEqual(o1.orderid, o2.orderid)
 
     def test_side(self):
         """
         Order.side must be either Side.BUY or Side.SELL.
         """
-        o1 = Order(0, Side.BUY, 4.5, 10)        
+        o1 = Order(0, Side.BUY, 4.5, 10)
         o2 = Order(0, Side.SELL, 4.5, 10)
-        self.assertEquals(o1.side, Side.BUY)
-        self.assertEquals(o2.side, Side.SELL)
+        self.assertEqual(o1.side, Side.BUY)
+        self.assertEqual(o2.side, Side.SELL)
         self.assertRaises(AssertionError, Order, 0, -1, 4.5, 10)
 
     def test_price(self):
         """
         Order.price must not be negative.
         """
-        o1 = Order(0, Side.BUY, 4.5, 10)        
+        o1 = Order(0, Side.BUY, 4.5, 10)
         o2 = Order(0, Side.BUY, 0.0, 10)
-        self.assertEquals(o1.price, 4.5)
-        self.assertEquals(o2.price, 0.0)
+        self.assertEqual(o1.price, 4.5)
+        self.assertEqual(o2.price, 0.0)
         self.assertRaises(AssertionError, Order, 0, Side.BUY, -4.5, 10)
 
     def test_volume(self):
         """
         Order.volume must not be negative.
         """
-        o1 = Order(0, Side.BUY, 4.5, 10)        
+        o1 = Order(0, Side.BUY, 4.5, 10)
         o2 = Order(0, Side.BUY, 4.5, 0)
-        self.assertEquals(o1.volume, 10)
-        self.assertEquals(o2.volume, 0)
+        self.assertEqual(o1.volume, 10)
+        self.assertEqual(o2.volume, 0)
         self.assertRaises(AssertionError, Order, 0, Side.BUY, 4.5, -10)
